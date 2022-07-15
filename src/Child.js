c6954b8a104e9749cb12ae928623cadf4ed72ef8
Ezra-code <njerukithinji872@gmail.com>
2022-07-15 06:05:07 +0300

    information flow in react

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  console.log(color)
+  // console.log(onChangeColor)
+
+  function HandleClick() {
+    const newColor = getRandomColor()
+    onChangeColor(newColor)
+  }
+  return <div onClick={HandleClick} className="child" style={{ backgroundColor: "#FFF" }} />;
 }
 
 export default Child;
